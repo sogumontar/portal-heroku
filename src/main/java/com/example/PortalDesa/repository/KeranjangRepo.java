4c23128fed86646a7454caf805c0a6e159f26e1c
unknown <hendrasimz92@gmail.com>
2020-05-25 12:14:07 +0700

    fix for web

--- src/main/java/com/example/PortalDesa/repository/KeranjangRepo.java
+++ src/main/java/com/example/PortalDesa/repository/KeranjangRepo.java
@@ -23,13 +23,13 @@ public interface KeranjangRepo extends JpaRepository<Keranjang,String> {
     @Query("SELECT k from Keranjang k where k.status=1 AND  k.idCustomer LIKE ?1")
     List findAllBySkuCustomer(String skuCustomer);
 
-    @Query("SELECT k, p from Keranjang k JOIN ProdukDesa p  ON k.idProduk where k.status=1 AND  k.idCustomer LIKE ?1")
+    @Query("SELECT k, p from Keranjang k JOIN ProdukDesa p  ON k.idProduk LIKE p.sku where k.status=1 AND  k.idCustomer LIKE ?1")
     List findAllBySkuCustomerForWeb(String skuCustomer);
 
     @Query("SELECT k from Keranjang k where k.status=1 AND  k.skuDesa LIKE ?1")
     List findAllByMerchant(String skuDesa);
 
-    @Query("SELECT k , p from Keranjang k  JOIN ProdukDesa p  ON k.idProduk where k.status=1 AND  k.skuDesa LIKE ?1")
+    @Query("SELECT k , p from Keranjang k  JOIN ProdukDesa p  ON k.idProduk LIKE p.sku where k.status=1 AND  k.skuDesa LIKE ?1")
     List findAllByMerchantForWeb(String skuDesa);
 
     @Query("SELECT COUNT(k.id) from Keranjang k where k.idProduk LIKE ?1 AND  k.idCustomer LIKE ?2 AND k.status=1")
